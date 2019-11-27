1f5dbaaceaf352578748331187e96c5f94c18ca3
Edxio Kraudy <kraudym.edxio@gmail.com>
2019-11-27 15:07:02 -0500

    first commit

---
+++ practice.py
@@ -0,0 +1,20 @@
+"""
+    practice.py
+    Created by: Edxio Kraudy Mora
+    Date: November 27, 2019
+"""
+
+#Printing Greetings
+print("Hello, World!");
+
+#x is a type of integer
+x = int (12)
+
+#String variables can be declared with either single or double quotes.
+y, z = 'Edxio ', "Kraudy"
+
+combine = y + z 
+
+print(x)
+print("My name is: " + combine)
+
