8697161db07f80b529932918bb5cf084fadbaf95
Edxio Kraudy <46289177+kraudym-edxio@users.noreply.github.com>
2020-01-29 19:38:30 -0500

    Add files via upload

---
+++ lab1_110006224.py
@@ -0,0 +1,10 @@
+"""
+    Lab 1 - Print Statement Exercise
+    Written by: Edxio Kraudy Mora (110006224)
+    Date: January 15, 2020
+"""
+
+print ("**aaaaaaaaaaaaaaaaaaaaaaaaa**")
+print ("**aaabbbbbbbbbbbbbbbbbbbaaa**")
+print ("**aaabbbbbbbbbbbbbbbbbbbaaa**")
+print ("**aaaaaaaaaaaaaaaaaaaaaaaaa**")
---
+++ lab2_110006224.py
@@ -0,0 +1,24 @@
+"""
+
+Programming for Beginners: Lab 2
+Written by: Edxio Kraudy Mora (110006224)
+Date: January 22, 2020
+
+PART 1: Pseudocode for Flowchart:
+
+	0. Start
+	1. Declare Integer favNumber
+	2. Display "What's your favorite number?"
+	3. Input favNumber
+	4. Display favNumber, " is a great number!"
+	5. Stop
+
+PART 2: Drawing Flowchart based on Pseudocode: (in separate PDF file)
+
+"""
+
+#Part 1 Code
+
+favNumber = int (input("What's your favourite number? "))
+
+print('%d is a great number!' %favNumber)
